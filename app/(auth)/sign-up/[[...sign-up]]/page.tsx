2c7798e69cf42099281465eff38e2ccdc731f125
unique-stha <unique-shrestha@outlook.com>
2024-11-15 20:20:29 -0500

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className='flex min-h-screen items-center justify-center gap-10'>
+    <div className='flex min-h-screen items-center justify-center'>
       <h1 className='text-4xl font-bold mt-20'>Please Sign Up</h1>
       <SignUp />
     </div>
